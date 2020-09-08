03f2a47a9ff67510a91bdbc87aeced40485321ee
TL Chang <coooo77@hotmail.com>
2020-09-08 21:43:06 +0800

    [新增] Users 頁面 dummyData

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -3,7 +3,52 @@
 </template>
 
 <script>
+const dummyUser = {
+  gender: "female",
+  name: {
+    title: "Ms",
+    first: "Ege",
+    last: "Aykaç",
+  },
+  location: {
+    street: {
+      number: 1666,
+      name: "Fatih Sultan Mehmet Cd",
+    },
+    city: "İstanbul",
+    state: "Denizli",
+    country: "Turkey",
+    postcode: 74279,
+    coordinates: {
+      latitude: "74.0895",
+      longitude: "149.7724",
+    },
+    timezone: {
+      offset: "-2:00",
+      description: "Mid-Atlantic",
+    },
+  },
+  email: "ege.aykac@example.com",
+  picture: {
+    large: "https://randomuser.me/api/portraits/women/6.jpg",
+    medium: "https://randomuser.me/api/portraits/med/women/6.jpg",
+    thumbnail: "https://randomuser.me/api/portraits/thumb/women/6.jpg",
+  },
+};
 export default {
-  name: '[View] Users'
-}
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    this.fetchUser();
+  },
+  methods: {
+    fetchUser() {
+      this.users.push(dummyUser);
+    },
+  },
+  name: "View-Users",
+};
 </script>
