ebe7b90edfa4444f0f261d5e0b87d84532c32fc1
TL Chang <coooo77@hotmail.com>
2020-09-08 21:34:56 +0800

    [新增] Users頁面與相關路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Users from '../views/Users'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: 'users'
+  },
+  {
+    path: '/users',
+    name: 'users',
+    component: Users
+  },
   {
     path: '*',
     name: 'not-found',
     component: () => import('../views/NotFound.vue')
-  },
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Users.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Users Page</h1>
+</template>
+
+<script>
+export default {
+  name: '[View] Users'
+}
+</script>
