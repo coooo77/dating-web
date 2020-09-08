b68a621b031a1f4b0686f5062cc330b936e0c12a
TL Chang <coooo77@hotmail.com>
2020-09-08 17:22:16 +0800

    [新增] Navbar、body的css variable設定

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,57 @@
 <template>
   <div id="app">
-    <h1>home page</h1>
+    <Navbar />
     <router-view />
   </div>
 </template>
 
 <script>
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "../src/components/Navbar";
 
 export default {
-  
-}
+  components: {
+    Navbar,
+  },
+};
 </script>
+
+<style>
+:root {
+  font-size: 16px;
+  font-family: "Open Sans";
+  --text-primary: #b6b6b6;
+  --text-secondary: #ececec;
+  --bg-primary: #23232e;
+  --bg-secondary: #141418;
+  --transition-speed: 600ms;
+}
+
+/* customize scroll bar */
+body::-webkit-scrollbar {
+  width: 0.25rem;
+}
+
+body::-webkit-scrollbar-track {
+  background: #1e1e24;
+}
+
+body::-webkit-scrollbar-thumb {
+  background: #6649b8;
+}
+
+.dark {
+  --text-primary: #b6b6b6;
+  --text-secondary: #ececec;
+  --bg-primary: #23232e;
+  --bg-secondary: #141418;
+}
+
+.light {
+  --text-primary: #576e75;
+  --text-secondary: #35535c;
+  --bg-primary: #fdf6e3;
+  --bg-secondary: #f5e5b8;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,162 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar-nav">
+      <li class="logo">
+        <a href="#" class="nav-link">
+          <span class="link-text logo-text">DATING WEBSITE</span>
+          <i class="fas fa-angle-double-right"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="fas fa-home"></i>
+          <span class="link-text">首頁</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="fas fa-search"></i>
+          <span class="link-text">搜尋</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="fas fa-kiss-wink-heart"></i>
+          <span class="link-text">最愛</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="fas fa-moon"></i>
+          <span class="link-text">模式</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  position: fixed;
+  background-color: var(--bg-primary);
+  transition: width 100ms ease;
+  padding: 0;
+  /* border:solid red 1px; */
+}
+
+.navbar-nav {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;    
+  /* border:solid green 1px; */
+}
+
+.nav-item {
+  width: 100%;
+  font-size: 1.5rem;
+  font-weight: bold;  
+}
+
+.nav-item:last-child {
+  margin-top: auto;
+}
+
+.nav-link {
+  display: flex;
+  align-items: center;
+  height: 5rem;
+  color: var(--text-primary);
+  text-decoration: none;
+  /* 讓選單變成灰色、透明度70% */
+  filter: grayscale(100%) opacity(0.7);
+  transition: var(--transition-speed);
+}
+
+.nav-link:hover {
+  /* 讓選單變成灰色、透明度70% */
+  filter: grayscale(0%) opacity(1);
+  background: var(--bg-secondary);
+  color: var(--text-secondary);
+}
+
+.nav-link i {
+  font-size: 2rem;
+  width: 2rem;
+  min-width: 2rem;
+  margin: 0 1.5rem;
+}
+
+.nav-link:hover {
+  color: #ff7eee;
+  transition: var(--transition-speed);
+}
+
+.link-text {
+  display: none;
+  margin-left: 1.5rem;
+}
+
+.logo {
+  font-weight: bold;
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+  text-align: center;
+  color: var(--text-secondary);
+  background: var(--bg-secondary);
+  font-size: 1.5rem;
+  letter-spacing: 0.3ch;
+  width: 100%;
+  transition: var(--transition-speed);
+}
+
+/* 讓LOGO旋轉 */
+.logo i {
+  transform: rotate(0deg);
+  transition: var(--transition-speed);
+}
+
+.navbar:hover .logo i {
+  transform: rotate(-180deg);
+}
+
+@media only screen and (max-width: 600px) {
+  .navbar {
+    height: 5rem;
+    width: 100vh;
+    bottom: 0;
+  }
+
+  .logo {
+    display: none;
+  }
+
+  .navbar-nav {
+    flex-direction: row;
+    width: 100vw;
+  }
+
+  .nav-link {
+    justify-content: center;
+  }
+}
+
+@media only screen and (min-width: 600px) {
+  .navbar {
+    width: 5rem;
+    height: 100vh;
+    top: 0;
+  }
+
+  .navbar:hover {
+    width: 16rem;
+  }
+
+  .navbar:hover .link-text {
+    display: inline;
+  }
+}
+</style>
