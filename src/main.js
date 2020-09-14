836da9120e0abae674f85d4df75eef01d963fb27
TL Chang <coooo77@hotmail.com>
2020-09-15 00:05:47 +0800

    [新增] jQuery

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import jQuery from 'jquery'
+window.$ = window.jQuery = jQuery
 
 Vue.config.productionTip = false
 
