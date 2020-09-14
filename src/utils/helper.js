6536c7b2ef81838435e869669f795c1253bd6414
TL Chang <coooo77@hotmail.com>
2020-09-14 23:32:31 +0800

    [新增] sweetalert2在helper中

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const baseURL = 'https://randomuser.me/api'
 
 const axiosInstance = axios.create({ baseURL })
 
-export { axiosInstance }
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true
+})
+
+export { axiosInstance, Toast }
